9874e3e3f7b6ebd459da54fe63607e1242c0f7df
Farhan <syedfarhan3599@gmail.com>
2026-04-30 13:44:22 +0530

    Updated the High-Impact SSR-like Optimizations

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import "./styles/global.css";
 import "./styles/premium.css";
 import "./styles/Homepage.css";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+const container = document.getElementById("root");
 
+if (container.hasChildNodes()) {
+  hydrateRoot(
+    container,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Register service worker for offline caching and fast loads
+serviceWorkerRegistration.register();
